eea234e38e028f0e76b4e2411c1131ee3217e6b8
Pier-Luc Gendreau <pier_taz@yahoo.com>
2016-12-30 12:20:34 -0500

    Firebase integration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Rebase from 're-base'
 import App from './App'
 import './index.css'
 
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyBH3i6QPS7Dd1Bjs-fdO-Q5oUPMhPtYiPM',
+  authDomain: 'form-pierluc-io-1642a.firebaseapp.com',
+  databaseURL: 'https://form-pierluc-io-1642a.firebaseio.com',
+  storageBucket: 'form-pierluc-io-1642a.appspot.com',
+  messagingSenderId: '1040118765063'
+})
+
 ReactDOM.render(
-  <App />,
+  <App base={base} />,
   document.getElementById('root')
 )
