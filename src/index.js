8f8673f50fdde7423446883e145adb18413cbbe3
Pier-Luc Gendreau <pierluc@outlook.com>
2017-01-04 22:52:01 -0500

    Integrate react-intl

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
+import { IntlProvider, addLocaleData } from 'react-intl'
+import en from 'react-intl/locale-data/en'
+
+import localeData from '../messages/data.json'
+
+addLocaleData([...en])
+
 import './index.css'
 
 import App from './App'
@@ -13,15 +20,21 @@ import Browse from './Browse'
 import Create from './Create'
 import Profile from './Profile'
 
+const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0]
+const messages = localeData[languageWithoutRegionCode] || localeData[language] || localeData.en
+
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={Home} />
-      <Route path='browse' component={Browse} />
-      <Route path='create' component={Create} />
-      <Route path='me' component={Profile} />
-      <Route path='connect' component={Login} />
-      <Route path='disconnect' component={Logout} />
-    </Route>
-  </Router>
+  <IntlProvider locale={language} messages={messages}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home} />
+        <Route path='browse' component={Browse} />
+        <Route path='create' component={Create} />
+        <Route path='me' component={Profile} />
+        <Route path='connect' component={Login} />
+        <Route path='disconnect' component={Logout} />
+      </Route>
+    </Router>
+  </IntlProvider>
 ), document.getElementById('root'))
