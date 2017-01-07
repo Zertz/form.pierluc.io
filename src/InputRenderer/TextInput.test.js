e4ccb669f54a7045d659963438a9a372f0f17d5a
Pier-Luc Gendreau <pierluc@outlook.com>
2017-01-07 15:26:47 -0500

    refactor(app): decouple InputRenderer

---
+++ src/InputRenderer/TextInput.test.js
@@ -0,0 +1,10 @@
+/* global it */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TextInput from './TextInput'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<TextInput />, div)
+})
