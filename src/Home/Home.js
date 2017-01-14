d36b8edf6ee16231b67da77ab0ccda100ef81ee2
Pier-Luc Gendreau <pierluc@outlook.com>
2017-01-14 14:50:27 -0500

    Remove demo code

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,6 @@ import {FormattedMessage} from 'react-intl'
 
 import './Home.css'
 
-import AddItem from '../AddItem.js'
 import Form from '../Form'
 import FormService from '../FormService'
 
@@ -12,41 +11,10 @@ class Home extends Component {
     super(props)
 
     this.state = {
-      todos: [],
-      isLoading: true,
       isDemoFormRendered: false
     }
 
     this.onGetStartedClicked = this.onGetStartedClicked.bind(this)
-  }
-
-  componentDidMount () {
-    const { base } = this.props
-
-    this.ref = base.syncState('todoList', {
-      context: this,
-      state: 'todos',
-      asArray: true,
-      then () {
-        this.setState({
-          isLoading: false
-        })
-      }
-    })
-  }
-
-  componentWillUnmount () {
-    const { base } = this.props
-
-    base.removeBinding(this.ref)
-  }
-
-  handleAddItem (todo) {
-    const { todos } = this.state
-
-    this.setState({
-      todos: todos.concat([todo])
-    })
   }
 
   onGetStartedClicked () {
@@ -56,21 +24,13 @@ class Home extends Component {
   }
 
   render () {
-    const { todos, isLoading, isDemoFormRendered } = this.state
+    const { isDemoFormRendered } = this.state
 
     return (
       <div className='Home'>
         <div className='HomeTitle Title'>
           <FormattedMessage id='Home.CraftFantasticForms' defaultMessage='Craft fantastic forms.' />
         </div>
-        <AddItem add={this.handleAddItem.bind(this)} />
-        <ul>
-          { isLoading
-            ? <li className='HomeLoading'>
-              <FormattedMessage id='Home.Loading' defaultMessage='Loading' />
-            </li>
-            : todos.map((todo, index) => <li key={index}>{todo}</li>) }
-        </ul>
         <div className='HomeSubtile Subtitle'>
           <FormattedMessage id='Home.SoHowEasyIsit' defaultMessage='So, how easy is it?' />
         </div>
