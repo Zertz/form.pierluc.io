4a4b71af4b78fd5dd317bf35221abb48a3249a2b
Pier-Luc Gendreau <pierluc@outlook.com>
2017-01-15 22:05:53 -0500

    Remove old, test stuff

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,41 +3,13 @@ import {FormattedMessage} from 'react-intl'
 
 import './Home.css'
 
-import Form from '../Form'
-import FormService from '../FormService'
-
 class Home extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      isDemoFormRendered: false
-    }
-
-    this.onGetStartedClicked = this.onGetStartedClicked.bind(this)
-  }
-
-  onGetStartedClicked () {
-    this.setState({
-      isDemoFormRendered: true
-    })
-  }
-
   render () {
-    const { isDemoFormRendered } = this.state
-
     return (
       <div className='Home'>
         <div className='HomeTitle Title'>
           <FormattedMessage id='Home.CraftFantasticForms' defaultMessage='Craft fantastic forms.' />
         </div>
-        <div className='HomeSubtile Subtitle'>
-          <FormattedMessage id='Home.SoHowEasyIsit' defaultMessage='So, how easy is it?' />
-        </div>
-        <button onClick={this.onGetStartedClicked}>
-          <FormattedMessage id='Home.GetStarted' defaultMessage='Get started' />
-        </button>
-        { isDemoFormRendered && <Form inputs={FormService.getDefault()} /> }
       </div>
     )
   }
