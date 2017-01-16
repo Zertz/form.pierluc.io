6ea2a6ffb59d79485fe488f069b4d82b6112febd
Pier-Luc Gendreau <pierluc@outlook.com>
2017-01-15 22:06:35 -0500

    Create Form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,15 +1,55 @@
 import React, {Component, PropTypes} from 'react'
 
-import InputRenderer from './InputRenderer'
+import './Form.css'
+
+import FieldRenderer from '../FieldRenderer'
+
+import PaymentService from '../PaymentService'
 
 class Form extends Component {
   constructor (props) {
     super(props)
 
+    this.state = {
+      checkout: PaymentService.initialize({
+        isLive: true,
+        checkoutCallback: this.checkoutCallback
+      })
+    }
+
     this.onSubmit = this.onSubmit.bind(this)
   }
 
+  async checkoutCallback (token) {
+    const { base } = this.props
+
+    try {
+      const userToken = await base.auth().currentUser.getToken()
+      const stripeToken = token.id
+      const formId = this.props.form.id
+
+      const charge = await PaymentService.charge({
+        formId,
+        stripeToken,
+        userToken
+      })
+
+      console.info(charge)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   onSubmit (e) {
+    const { checkout } = this.state
+
+    checkout.open({
+      name: 'form.pierluc.io',
+      description: 'hello',
+      currency: 'cad',
+      amount: 2000
+    })
+
     e.preventDefault()
   }
 
@@ -18,7 +58,7 @@ class Form extends Component {
 
     return (
       <form className='Form' onSubmit={this.onSubmit}>
-        { inputs.map((input, index) => <InputRenderer key={index} input={input} />) }
+        {inputs.map((input, index) => <FieldRenderer key={index} input={input} />)}
       </form>
     )
   }
---
+++ src/Form/Form.test.js
@@ -0,0 +1,10 @@
+/* global it */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Form from './Form'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Form />, div)
+})
