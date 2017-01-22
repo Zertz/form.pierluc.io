cb938acb63c15ca980008713728c632ef86cd0ca
Pier-Luc Gendreau <pierluc@outlook.com>
2017-01-22 16:45:34 -0500

    getUserLocation

--- src/AppService/AppService.js
+++ src/AppService/AppService.js
@@ -8,6 +8,36 @@ class AppService {
   getRandomId () {
     return uuid.v4()
   }
+
+  getUserLocation () {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const queryParams = {
+        username: 'form_pierluc_io',
+        type: 'json',
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+
+      const queryString = Object.keys(queryParams).map((key) => {
+        return `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`
+      }).join('&')
+
+      const request = new window.Request(`http://ws.geonames.org/countryCode?${queryString}`, {
+        method: 'post',
+        mode: 'cors',
+        credentials: 'omit'
+      })
+
+      try {
+        const response = await window.fetch(request)
+        const json = await response.json()
+
+        console.info(json)
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  }
 }
 
 const appService = new AppService()
