b781e9c51b7c68dfdb9a8e5c7b35dffe5c375d50
Pier-Luc Gendreau <pierluc@outlook.com>
2017-02-09 21:40:19 -0500

    Footer fixes

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -7,7 +7,7 @@ import Text from '../Text'
 function Footer () {
   return (
     <div className='Footer'>
-      <Text content={'©️ pierluc.io'} />
+      <Text>©️ pierluc.io</Text>
     </div>
   )
 }
